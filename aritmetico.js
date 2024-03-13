b3402ab6f5535b2374e53897c06c7bca39037a74
Mariana Segovia Penha <158625854+maripenha@users.noreply.github.com>
2024-03-13 19:40:40 +0000

    testejs

---
+++ aritmetico.js
@@ -0,0 +1,6 @@
+const anoAtual = 2024 
+const anoNascimento = 1997
+
+const idadeRenatha = anoAtual - anoNascimento
+
+console.log(`A idade da srta. Renatha é de: ${idadeRenatha} anos`)
---
+++ first.js
@@ -0,0 +1,23 @@
+// concatenação, interpolação, literal templates
+
+console.log("Hello World!")
+
+const corDoCarro = "Vermelho"
+const modeloDoCarro = "Sandero"
+const marcaDoCarro = "Renault"
+const chassiDoCarro = 93930203903030
+const anoDoCarro = 2017
+
+console.log(" Cor predominante: ", corDoCarro, " Modelo do carro: ", modeloDoCarro, 
+" Marca: ", marcaDoCarro, " Chassi: ", chassiDoCarro, " Ano: ", anoDoCarro)
+
+console.log(`Cor predominante: ${corDoCarro}, Modelo do carro: ${modeloDoCarro},
+ Marca: ${marcaDoCarro}, Chassi: ${chassiDoCarro}, Ano: ${anoDoCarro}`)
+
+ console.log(` Bem vindo ao bar do Guilherme. Escolha uma opção:
+    1- litrão
+    2- refri
+    3- água
+    4- vodka
+ `)
+ 
